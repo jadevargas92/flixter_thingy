fcca51352201642cfd39e07cd5b0fcd547cd3a30
Jade Vargas <jadevargas92@gmail.com>
2016-04-07 17:42:30 +0000

    Added CC functionality, need to do video still

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,8 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_course, only: [:show]
+
+	
 	def show
 		@photo = Photo.new
 		@course = Course.new
@@ -7,6 +11,17 @@ class LessonsController < ApplicationController
 	private
 	helper_method :current_lesson
 	def current_lesson
-    	@current_lesson ||= Lesson.find(params[:id])
-  	end
+		@current_lesson ||= Lesson.find(params[:id])
+	end
+
+	def require_authorized_for_current_course
+		if current_user.enrolled_in?(current_lesson.section.course) != true
+			redirect_to :back, :alert => "You must be enrolled to view the lesson!"
+		end
+	end
+
+	helper_method :current_course
+	def current_course
+		@current_course ||= current_lesson.section.course
+	end
 end
